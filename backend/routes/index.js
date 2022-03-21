f1b31326587386afc6dda0e2db2ec6d57119bc2c
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-03-21 17:56:34 -0400

    add backend
    Added all local files to the backend folder

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res, next) {
+  res.json({body: 'hello world'});
+});
+
+module.exports = router;
