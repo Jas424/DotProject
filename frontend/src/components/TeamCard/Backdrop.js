02d3795a10ac30496b73c64f2d04477a780c3d9e
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-04-11 19:54:34 -0400

    V 3.1

--- frontend/src/components/TeamCard/Backdrop.js
+++ frontend/src/components/TeamCard/Backdrop.js
@@ -1,6 +1,8 @@
 /* this is a component of TeamMemberCard.js. i'm planning to use this to display a backdrop when the "learn more" button is pressed */
+import classes from "./Backdrop.module.css";
+
 function Backdrop(props) {
-  return <div className="backdrop" onClick={props.onCancel} />;
+  return <div className={classes.card} onClick={props.onCancel} />;
 }
 
 export default Backdrop;
