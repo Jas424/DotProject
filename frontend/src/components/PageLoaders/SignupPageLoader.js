dcd21b67a29ddf645839f13cbb8d7e38dd2e35d7
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-04-12 09:49:24 -0400

    V 3.1

--- frontend/src/components/PageLoaders/SignupPageLoader.js
+++ frontend/src/components/PageLoaders/SignupPageLoader.js
@@ -1,17 +1,22 @@
+/*This component works as a container that will hold the actual contents of the signup page. */
+
 import React from "react";
 import SignupForm from "../SignupForm";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../../contexts/AuthContext";
 
 function SignupPageLoader() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={({ minHeight: "100vh" }, { minWidth: "100vh" })}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <SignupForm />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={({ minHeight: "100vh" }, { minWidth: "100vh" })}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignupForm />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,16 +1,39 @@
-import React, { useRef } from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 function SignupForm() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match. Please try again.");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account.");
+    }
+    setLoading(false);
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email" controlId="formEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
@@ -31,7 +54,7 @@ function SignupForm() {
             </Form.Group>
 
             <p></p>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
