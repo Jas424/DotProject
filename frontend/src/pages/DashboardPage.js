0cf36de125757779240ab6c44375f85ee7b8935d
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-04-14 03:52:22 -0400

    Update DashboardPage.js

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  const [error, setError] = useState("");
+  const currentUser = useAuth();
+  function handleLogout() {}
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">PROFILE</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong> {currentUser.email}
+          {currentUser.email}
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/signup">
+          <Button variant="link" onClick={handleLogout}>
+            LOG OUT
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }
