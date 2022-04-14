e3903cf928bb4e92c454b3afb2f17eabf5bcc891
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-04-14 07:15:02 -0400

    V 3.8

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Alert, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
 
 function DashboardPage() {
   const [error, setError] = useState("");
@@ -9,21 +9,22 @@ function DashboardPage() {
   function handleLogout() {}
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">PROFILE</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email: </strong> {currentUser.email}
-          {currentUser.email}
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Link to="/login">
-          <Button variant="link" onClick={handleLogout}>
-            LOG OUT
-          </Button>
-        </Link>
-      </div>
+      <AuthProvider>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">PROFILE</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email: </strong> Text {currentUser && currentUser.email}
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Link to="/login">
+            <Button variant="link" onClick={handleLogout}>
+              <Alert>LOG OUT</Alert>
+            </Button>
+          </Link>
+        </div>
+      </AuthProvider>
     </>
   );
 }
