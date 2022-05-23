412a6f10728a8b0f6e59a987b6f1cbbd8f18c550
agenor <agenor.rodriguez@jjay.cuny.edu>
2022-05-23 13:36:36 -0400

    V6.4
    updated image links in about us page to online rather than local

--- frontend/src/components/TeamCard/Backdrop.js
+++
@@ -1,8 +0,0 @@
-/* this is a component of TeamMemberCard.js. i'm planning to use this to display a backdrop when the "learn more" button is pressed */
-import classes from "./Backdrop.module.css";
-
-function Backdrop(props) {
-  return <div className={classes.card} onClick={props.onCancel} />;
-}
-
-export default Backdrop;
--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -3,12 +3,15 @@ import { Alert, Card } from "react-bootstrap";
 
 //images
 import dotSmallLogo from "../components/layout/dot-small-logo.png";
-import ironman from "../components/images/ironman.jpg";
-import blackwidow from "../components/images/blackwidow.jpg";
-import captainamerica from "../components/images/captainamerica.jpg";
-import spiderman from "../components/images/spiderman.png";
-import hulk from "../components/images/hulk.jpg";
-import gamora from "../components/images/gamora.png";
+
+let avatars = [
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fironman.jpg?alt=media&token=255b1ffd-49e7-4bf6-844e-afcccdac9865",
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fgamora.png?alt=media&token=bd5c8858-5493-40b4-a534-8790ad345684",
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fcaptainamerica.jpg?alt=media&token=14e6b637-1ac4-4c82-91d2-449d5268c4ec",
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fspiderman.png?alt=media&token=5203de70-24ac-4a35-be71-80968dc41e95",
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fblackwidow.jpg?alt=media&token=ab26d7b9-5c88-4152-a1c6-d8a5d9a285c3",
+  "https://firebasestorage.googleapis.com/v0/b/dot-auth-dev.appspot.com/o/profile-photos%2Fhulk.jpg?alt=media&token=bc775d03-0c32-4c0b-98df-d7a47ff00a63",
+];
 
 function AboutUsPage() {
   return (
@@ -20,59 +23,72 @@ function AboutUsPage() {
           <h1>Meet The Dot Team</h1>
         </center>
       </Alert>
-      <div className="flexbox-container">
-        {/* <div className="card1">
-          <TeamMemberCard
-            fullName="Agenor Rodriguez"
-            teamPhoto={genericMalePhoto}
-          />
-        </div> */}
 
+      {/* wrap all the cards in a flexbox container */}
+      <div className="flexbox-container">
+        {/* Agenor's Card */}
         <div className="card1">
           <Card>
-            <Card.Img variant="top" src={ironman} />
+            <Card.Img variant="top" src={avatars[0]} />
             <Card.Body></Card.Body>
-            <Card.Footer as="h3">Agenor Rodriguez</Card.Footer>
+            <Card.Footer as="h3">
+              <center>Agenor Rodriguez</center>
+            </Card.Footer>
           </Card>
         </div>
 
-        <div className="card2">
-          <Card>
-            <Card.Header as="h3">Jade Wing Ki Au</Card.Header>
-            <Card.Img variant="bottom" src={blackwidow} />
-            <Card.Body></Card.Body>
-          </Card>
-        </div>
-
+        {/* Jas's Card */}
         <div className="card3">
           <Card>
-            <Card.Img variant="top" src={captainamerica} />
+            <Card.Header as="h3">
+              <center>Jaspreet Singh</center>
+            </Card.Header>
+            <Card.Img variant="bottom" src={avatars[2]} />
             <Card.Body></Card.Body>
-            <Card.Footer as="h3">Jaspreet Singh</Card.Footer>
           </Card>
         </div>
 
+        {/* Jonathan's Card */}
         <div className="card4">
           <Card>
-            <Card.Img variant="top" src={spiderman} />
+            <Card.Img variant="top" src={avatars[3]} />
             <Card.Body></Card.Body>
-            <Card.Footer as="h3">Jonathan Yanez</Card.Footer>
+            <Card.Footer as="h3">
+              <center>Jonathan Yanez</center>
+            </Card.Footer>
           </Card>
         </div>
 
+        {/* Jade's Card */}
+        <div className="card2">
+          <Card>
+            <Card.Header as="h3">
+              <center>Jade Wing Ki Au</center>
+            </Card.Header>
+            <Card.Img variant="bottom" src={avatars[1]} />
+            <Card.Body></Card.Body>
+          </Card>
+        </div>
+
+        {/* Varsha's Card */}
         <div className="card5">
           <Card>
-            <Card.Header as="h3">Jiten Lama</Card.Header>
-            <Card.Img variant="bottom" src={hulk} />
+            <Card.Img variant="top" src={avatars[4]} />
             <Card.Body></Card.Body>
+            <Card.Footer as="h3">
+              <center>Varsha Singh</center>
+            </Card.Footer>
           </Card>
         </div>
 
+        {/* Jiten's Card */}
         <div className="card6">
           <Card>
-            <Card.Img variant="top" src={gamora} />
+            <Card.Header as="h3">
+              <center>Jiten Lama</center>
+            </Card.Header>
+            <Card.Img variant="bottom" src={avatars[5]} />
             <Card.Body></Card.Body>
-            <Card.Footer as="h3">Varsha Singh</Card.Footer>
           </Card>
         </div>
 
